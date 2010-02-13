24c59c8d299a9b227fbf252cbea1d90877c94cdc
yyang526 <yyang526@16613f8c-0d2e-11df-9821-6b6fb8eff287>
2010-02-13 04:53:34 +0000

    reorganize

---
+++ src/com/usyd/main/Main.java
@@ -0,0 +1,25 @@
+package com.usyd.main;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+
+    public static void main(String argv[]) throws Exception {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                MainFrame frm = new MainFrame();
+                frm.pack();
+                frm.setLocationRelativeTo(null);
+                frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frm.setVisible(true);
+            }
+        });
+    }
+}
